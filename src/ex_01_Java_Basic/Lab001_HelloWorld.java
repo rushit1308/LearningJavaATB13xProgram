e74f4eea4b4c3b5160f784c832092af1d2055095
Rushit <shethwise@fmail.com>
2025-06-11 07:49:02 +0530

    Done

--- src/ex_01_Java_Basic/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorld.java
@@ -2,6 +2,6 @@ package ex_01_Java_Basic;
 
 public class Lab001_HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, My name is Rushit Sheth");
+        System.out.println("Hello, My name is Rushit");
     }
 }
